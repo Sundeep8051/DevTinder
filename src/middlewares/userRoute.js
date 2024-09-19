8fbc1b86662a5abed8aa9df8c5e734bec21c3947
Sundeep <sundeepm1@gmail.com>
2024-09-19 12:00:33 +0530

    Added User model validations

--- src/middlewares/userRoute.js
+++ src/middlewares/userRoute.js
@@ -2,40 +2,69 @@ const User = require("../models/user");
 
 const userRoute = (app) => {
   app.post("/signup", async (req, res) => {
-    const user = new User(req.body);
+    try {
+      const user = new User(req.body);
 
-    await user.save();
-    res.send("User created successfully");
+      await user.save();
+      res.send("User created successfully");
+    } catch (err) {
+      res.status(400).send(`Error: ${err}`);
+    }
   });
 
   app.get("/feed/:firstname", async (req, res) => {
-    let name = req.params.firstname;
-    let user = await User.findOne({ firstName: name });
-    if (!user) res.status(404).send(`user with ${name} not found`);
-    else {
-      res.status(200).send(user);
+    try {
+      let name = req.params.firstname;
+      let user = await User.findOne({ firstName: name });
+      if (!user) res.status(404).send(`user with ${name} not found`);
+      else {
+        res.status(200).send(user);
+      }
+    } catch (err) {
+      res.status(400).send(`Error: ${err}`);
     }
   });
 
   app.get("/feed", async (req, res) => {
-    const users = await User.find({});
-    if (users.length === 0) {
-      res.status(404).send("no user found");
-    } else {
-      res.status(200).send(users);
+    try {
+      const users = await User.find({});
+      if (users.length === 0) {
+        res.status(404).send("no user found");
+      } else {
+        res.status(200).send(users);
+      }
+    } catch (err) {
+      res.status(400).send(`Error: ${err}`);
     }
   });
 
   app.patch("/user", async (req, res) => {
-    const userId = req.body.userId;
-    const data = req.body;
-    const user = await User.findByIdAndUpdate(userId, data, {
-      returnDocument: "after",
-    });
-    if (!user) {
-      res.status(404).send("User details not found");
+    try {
+      const userId = req.body.userId;
+      const data = req.body;
+      const user = await User.findByIdAndUpdate(userId, data, {
+        returnDocument: "after",
+      });
+      if (!user) {
+        res.status(404).send("User details not found");
+      }
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(400).send(`Error: ${err}`);
     }
-    res.status(200).send(user);
+  });
+
+  app.delete("/user", async (req, res) => {
+    try {
+      const userId = req.body.userId;
+      const user = await User.findByIdAndDelete(userId);
+      if (!user) {
+        res.status(404).send(`User with Id: ${userId} does not exist`);
+      }
+      res.status(200).send(`User: ${user.firstName} deleted successfully`);
+    } catch (err) {
+      res.status(400).send(`Error: ${err}`);
+    }
   });
 };
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
   lastName: String,
-  emailId: String,
+  emailId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   password: String,
-  age: Number,
-  gender: String,
+  age: { type: Number, min: 18, max: 99, required: true },
+  gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
