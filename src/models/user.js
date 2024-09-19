44bad5750d0b57ed44dd52effff17244090fa7a8
Sundeep <sundeepm1@gmail.com>
2024-09-19 12:44:02 +0530

    updated model validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,33 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: String,
+    emailId: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: String,
+    age: { type: Number, min: 18, max: 99, required: true },
+    gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      required: true,
+    },
+    about: {
+      type: String,
+      default: "This is the default desciption about the user",
+      required: true,
+    },
+    skills: [String],
   },
-  lastName: String,
-  emailId: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: String,
-  age: { type: Number, min: 18, max: 99, required: true },
-  gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
